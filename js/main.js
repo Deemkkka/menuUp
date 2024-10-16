2453d57f1f578fad07069b2ffb72aa0f23a89c7a
Deem <Deemkkka@mail.ru>
2024-10-16 10:59:23 +0300

    добавили в JS логику добавление класса active при клике, также изменение свойства атрибута aria-expanded

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
 
 });
+
+const button = document.querySelector('.button');
+
+ button.addEventListener('click', () => {
+    button.classList.toggle('active');
+    button.setAttribute('aria-expanded', button.classList.contains('active') ? 'true' : 'false');
+ });
